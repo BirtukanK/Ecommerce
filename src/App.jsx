a376c42cb1276a592b54981eb482cfb4f5012ba7
BirtukanK <birtukankuma1113@gmail.com>
2024-04-04 11:27:13 +0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -85,15 +85,21 @@ function App () {
       <div className="bg-primary w-full overflow-hidden">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-            <Navbar />
-            
+            <Navbar />  
           </div>
         </div>
         <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Hero />
+              <Product />
+              <Footer />
+            </div>
+          </div>
+        {/* <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
               <div className={`${styles.boxWidth}`}>
                 <Hero/>
               </div>
-            </div>
+            </div> */}
         <Routes>
           <Route path="/" element={<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
